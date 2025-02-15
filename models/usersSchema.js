fbf6867d21370d2bfa74eb614c7460d397a681d2
Anastasiya-Shtukar <162809005+Anastasiya-Shtukar@users.noreply.github.com>
2025-02-15 22:24:12 +0100

    upload

--- models/usersSchema.js
+++ models/usersSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const userSchema = new Schema({
   password: {
@@ -12,6 +13,7 @@ const userSchema = new Schema({
     required: [true, "Email is required"],
     unique: true,
   },
+  avatarURL: String,
   subscription: {
     type: String,
     enum: ["starter", "pro", "business"],
@@ -26,6 +28,14 @@ const userSchema = new Schema({
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.pre("save", function (next) {
+  if (this.isNew && !this.avatarURL) {
+    const avatar = gravatar.url(this.email, { s: "200", r: "pg", d: "mm" });
+    this.avatarURL = avatar;
+  }
   next();
 });
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,10 @@ const Joi = require("joi");
 const jwt = require("jsonwebtoken");
 const auth = require("../../models/auth.js");
 const User = require("../../models/usersSchema.js");
+const multer = require("multer");
+const jimp = require("jimp");
+const fs = require("fs");
+const path = require("path");
 
 const signupValidate = Joi.object({
   email: Joi.string().email().required(),
@@ -34,6 +38,7 @@ router.post("/signup", async (req, res) => {
       email,
       password,
       subscription: "starter",
+      avatarURL: "",
     });
 
     await newUser.save();
@@ -42,6 +47,7 @@ router.post("/signup", async (req, res) => {
       user: {
         email: newUser.email,
         subscription: newUser.subscription,
+        avatarURL: newUser.avatarURL,
       },
     });
   } catch (error) {
@@ -81,6 +87,7 @@ router.post("/login", async (req, res) => {
       user: {
         email: user.email,
         subscription: user.subscription,
+        avatarURL: user.avatarURL,
       },
     });
   } catch (error) {
@@ -120,6 +127,56 @@ router.get("/current", async (req, res) => {
     });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "tmp");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+}).single("avatar");
+
+router.patch("/avatars", upload, async (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+
+  try {
+    const { _id } = req.user;
+    const avatarPath = path.join(__dirname, "../../", "tmp", req.file.filename);
+
+    const avatar = await jimp.read(avatarPath);
+    await avatar.resize(250, 250);
+    const publicDir = path.resolve(__dirname, "../../", "public", "avatars");
+
+    if (!fs.existsSync(publicDir)) {
+      fs.mkdirSync(publicDir, { recursive: true });
+    }
+
+    const avatarFilename = `${_id}-${req.file.filename}`;
+    const avatarSavePath = path.join(publicDir, avatarFilename);
+
+    await avatar.write(avatarSavePath);
+
+    const avatarURL = `/avatars/${avatarFilename}`;
+    await User.findByIdAndUpdate(_id, { avatarURL });
+
+    fs.unlinkSync(avatarPath);
+
+    res.status(200).json({
+      avatarURL,
+    });
+  } catch (error) {
+    console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 });
